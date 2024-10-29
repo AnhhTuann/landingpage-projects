2abdb0b8c1c1f6ab89cbe0fe431175a2badf92e3
AnhhTuann <tuannguyen25101999@gmail.com>
2024-10-29 18:53:10 +0700

    add tuancoffee-landingpage

---
+++ tuancoffee-landingpage/scripts/app.js
@@ -0,0 +1,42 @@
+const toggle = document.querySelector(".menu-toggle");
+const menu = document.querySelector(".menu");
+const activeClass = "is-show";
+toggle.addEventListener("click", function () {
+  menu.classList.add(activeClass);
+});
+window.addEventListener("click", function (e) {
+  if (!menu.contains(e.target) && !e.target.matches(".menu-toggle")) {
+    menu.classList.remove(activeClass);
+  }
+});
+
+$(".testimonial-list").slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  dots: true,
+  prevArrow:
+    "<button type='button' class='slick-prev pull-left'><i class='fa-solid fa-arrow-left' aria-hidden='true'></i></button>",
+  nextArrow:
+    "<button type='button' class='slick-next pull-right'><i class='fa-solid fa-arrow-right' aria-hidden='true'></i></button>",
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        arrows: false,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        arrows: false,
+      },
+    },
+  ],
+});
